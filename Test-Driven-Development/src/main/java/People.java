734cb86ffe4dd66a0b3e8d0ef0ae5f83f18efc5a
Дмитрий Галашев <galashev1990@gmail.com>
2023-02-12 14:57:14 +0300

    add implemented

---
+++ Test-Driven-Development/src/main/java/People.java
@@ -0,0 +1,17 @@
+public class People {
+    private String name;
+    private String number;
+
+    public People(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+}
