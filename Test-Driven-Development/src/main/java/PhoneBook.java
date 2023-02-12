6366a5f4d7cadcd73a2a658ae52041dde71e6ba2
Дмитрий Галашев <galashev1990@gmail.com>
2023-02-12 14:53:44 +0300

    add implemented

--- Test-Driven-Development/src/main/java/PhoneBook.java
+++ Test-Driven-Development/src/main/java/PhoneBook.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class PhoneBook {
+    List<People> phonebook = new ArrayList<>();
     public int add(String name, String number) {
-    return 0;
+        phonebook.add(new People(name, number));
+        phonebook.sort(Comparator.comparing(People::getName));
+        return phonebook.size();
     }
 }
